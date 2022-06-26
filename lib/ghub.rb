c01f18471f582a704a047c104411e24939c3999f
Brooke Kuhlmann <brooke@alchemists.io>
2022-06-26 13:25:26 -0600

    Added Dry gems to gem boot
    Necessary to ensure the global configuration for the Dry gems is
    properly loaded for use throughout the entire project.

--- lib/ghub.rb
+++ lib/ghub.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
+require "dry/monads"
+require "dry/schema"
 require "zeitwerk"
 
+Dry::Schema.load_extensions :monads
 Zeitwerk::Loader.for_gem.setup
 
 # Main namespace.
