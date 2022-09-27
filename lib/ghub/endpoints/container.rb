4465ef16687151b03f4c9f8d27dc4b65bc4594c9
Brooke Kuhlmann <brooke@alchemists.io>
2022-09-27 09:14:27 -0600

    Added endpoints container
    Necessary to define all endpoint dependencies. This will soon be used
    by the primary client (not implemented yet).

---
+++ lib/ghub/endpoints/container.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "dry/container"
+
+module Ghub
+  module Endpoints
+    # Defines endpoint dependencies.
+    module Container
+      extend Dry::Container::Mixin
+
+      register(:branch_protection) { Endpoints::Branches::Protection::Root.new }
+      register(:branch_signature) { Endpoints::Branches::Signature::Root.new }
+      register(:organization_members) { Endpoints::Organizations::Members::Root.new }
+      register(:pulls) { Endpoints::Pulls::Root.new }
+      register(:repositories) { Endpoints::Repositories::Root.new }
+      register(:users) { Endpoints::Users::Root.new }
+    end
+  end
+end
