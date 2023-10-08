99e7a63716ae1f40c51a452c8301bcd21e37aa2e
Brooke Kuhlmann <brooke@alchemists.io>
2023-10-08 17:32:28 -0600

    Updated to Infusible 2.2.0
    Necessary to reduce the injection of private dependencies only to make them public. Now the endpoints can be exposed as public by default.
    
    Milestone: minor

--- lib/ghub/client.rb
+++ lib/ghub/client.rb
@@ -3,35 +3,21 @@
 module Ghub
   # The primary interface for making API requests.
   class Client
-    include Import[:configuration]
+    include Endpoints::Import.public(
+      :branch_protection,
+      :branch_signature,
+      :organization_members,
+      :pulls,
+      :repositories,
+      :search_users,
+      :users
+    )
 
-    include Endpoints::Import[
-      branch_protection_endpoint: :branch_protection,
-      branch_signature_endpoint: :branch_signature,
-      organization_members_endpoint: :organization_members,
-      pulls_endpoint: :pulls,
-      repositories_endpoint: :repositories,
-      search_users_endpoint: :search_users,
-      users_endpoint: :users
-    ]
+    include Import[:configuration]
 
     def initialize(**)
       super(**)
       yield configuration if block_given?
     end
-
-    def branch_protection = branch_protection_endpoint
-
-    def branch_signature = branch_signature_endpoint
-
-    def organization_members = organization_members_endpoint
-
-    def pulls = pulls_endpoint
-
-    def repositories = repositories_endpoint
-
-    def search_users = search_users_endpoint
-
-    def users = users_endpoint
   end
 end
